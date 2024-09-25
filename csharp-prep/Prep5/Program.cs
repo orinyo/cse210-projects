49367f3e6c70d119dbe0c396b1f621a4f61b3175
stephenorinyo <orinyostephen@gmail.com>
2024-09-25 03:52:11 +0300

    Added code to run functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,53 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Call the DisplayWelcome function to display the welcome message
+        DisplayWelcome();
+
+        // Call PromptUserName to get the user's name
+        string userName = PromptUserName();
+
+        // Call PromptUserNumber to get the user's favorite number
+        int favoriteNumber = PromptUserNumber();
+
+        // Call SquareNumber to calculate the square of the favorite number
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        // Call DisplayResult to show the user's name and the squared number
+        DisplayResult(userName, squaredNumber);
+    }
+
+    // Function to display a welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Function to prompt the user for their name and return it
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    // Function to prompt the user for their favorite number and return it as an integer
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    // Function to calculate and return the square of a number
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    // Function to display the user's name and the squared number
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
