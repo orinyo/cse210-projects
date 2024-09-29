b0bc361fd546e7b12a245a6b389777bb04fe366e
stephenorinyo <orinyostephen@gmail.com>
2024-09-29 05:36:14 +0300

    Added code

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,11 +1,38 @@
-public class Entry
-{
-    public string _date;
-    public string _promptText;
-    public string _entryText;
+using System;
 
-    public void Display()
+namespace DailyJournal
+{
+    class Entry
     {
-        
+        public string _prompt;
+        public string _response;
+        public string _date;
+
+        public Entry(string prompt, string response, string date)
+        {
+            _prompt = prompt;
+            _response = response;
+            _date = date;
+        }
+
+        public string GetPrompt()
+        {
+            return _prompt;
+        }
+
+        public string GetResponse()
+        {
+            return _response;
+        }
+
+        public string GetDate()
+        {
+            return _date;
+        }
+
+        public override string ToString()
+        {
+            return $"Date: {_date} - Prompt: {_prompt} \n{_response}\n";
+        }
     }
 }
