968672120a3f414c15c9f008d79a767c9795c285
stephenorinyo <orinyostephen@gmail.com>
2024-09-29 05:36:55 +0300

    Added Code

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Collections.Generic;
+
 public class PromptGenerator
 {
-    public List<string> _prompts;
+    public List<string> _prompts = new List<string>();
 
+    // Method to get a random prompt
     public string GetRandomPrompt()
     {
-        return "";
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
     }
 }
