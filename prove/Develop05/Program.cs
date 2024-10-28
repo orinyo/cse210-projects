6b5d747821e9a4cfd652e09d0a1c76ee42367de2
orinyostephen@gmail.com <orinyostephen@gmail.com>
2024-10-29 01:32:51 +0300

    Added mindfullness program code

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,183 @@
 using System;
+using System.Threading;
 
-class Program
+abstract class Activity
 {
-    static void Main(string[] args)
+    protected int duration;
+
+    public void Start()
     {
-        Console.WriteLine("Hello Develop05 World!");
+        DisplayStartingMessage();
+        PrepareToBegin();
+        RunActivity();
+        DisplayEndingMessage();
+    }
+
+    protected virtual void DisplayStartingMessage()
+    {
+        Console.WriteLine("Welcome to the mindfulness program.");
+        Console.WriteLine("This activity will guide you through a mindfulness exercise.");
+        Console.Write("Enter the duration in seconds: ");
+        duration = int.Parse(Console.ReadLine());
+    }
+
+    protected void PrepareToBegin()
+    {
+        Console.WriteLine("Prepare to begin...");
+        DisplaySpinner(3);
+    }
+
+    protected abstract void RunActivity();
+
+    protected virtual void DisplayEndingMessage()
+    {
+        Console.WriteLine("Good job! You completed the activity.");
+        Console.WriteLine($"Total duration: {duration} seconds.");
+        DisplaySpinner(3);
+    }
+
+    protected void DisplaySpinner(int seconds)
+    {
+        for (int i = 0; i < seconds * 2; i++)
+        {
+            Console.Write("/");
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+            Console.Write("-");
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+        }
+    }
+}
+
+class BreathingActivity : Activity
+{
+    protected override void DisplayStartingMessage()
+    {
+        Console.WriteLine("This activity will help you relax by guiding your breathing.");
+        base.DisplayStartingMessage();
+    }
+
+    protected override void RunActivity()
+    {
+        DateTime endTime = DateTime.Now.AddSeconds(duration);
+        while (DateTime.Now < endTime)
+        {
+            Console.WriteLine("Breathe in...");
+            DisplayCountdown(3);
+            Console.WriteLine("Breathe out...");
+            DisplayCountdown(3);
+        }
+    }
+
+    private void DisplayCountdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+}
+
+class ReflectionActivity : Activity
+{
+    private static readonly string[] Prompts = {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need."
+    };
+
+    private static readonly string[] Questions = {
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you feel when it was complete?"
+    };
+
+    protected override void DisplayStartingMessage()
+    {
+        Console.WriteLine("This activity will help you reflect on times when you showed resilience.");
+        base.DisplayStartingMessage();
+    }
+
+    protected override void RunActivity()
+    {
+        Random rand = new Random();
+        DateTime endTime = DateTime.Now.AddSeconds(duration);
+
+        Console.WriteLine(Prompts[rand.Next(Prompts.Length)]);
+        while (DateTime.Now < endTime)
+        {
+            Console.WriteLine(Questions[rand.Next(Questions.Length)]);
+            DisplaySpinner(3);
+        }
+    }
+}
+
+class ListingActivity : Activity
+{
+    private static readonly string[] Prompts = {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are your personal heroes?"
+    };
+
+    protected override void DisplayStartingMessage()
+    {
+        Console.WriteLine("This activity will help you reflect on positive aspects of your life.");
+        base.DisplayStartingMessage();
+    }
+
+    protected override void RunActivity()
+    {
+        Random rand = new Random();
+        DateTime endTime = DateTime.Now.AddSeconds(duration);
+
+        Console.WriteLine(Prompts[rand.Next(Prompts.Length)]);
+        int count = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("List an item: ");
+            Console.ReadLine();
+            count++;
+        }
+
+        Console.WriteLine($"You listed {count} items!");
+    }
+}
+
+class MindfulnessProgram
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("\nMindfulness Program Menu");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Exit");
+            Console.Write("Choose an option: ");
+
+            string choice = Console.ReadLine();
+            Activity activity = choice switch
+            {
+                "1" => new BreathingActivity(),
+                "2" => new ReflectionActivity(),
+                "3" => new ListingActivity(),
+                "4" => null,
+                _ => throw new InvalidOperationException("Invalid choice. Try again.")
+            };
+
+            if (activity == null)
+            {
+                Console.WriteLine("Exiting program.");
+                break;
+            }
+
+            activity.Start();
+        }
     }
 }
