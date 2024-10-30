d783ffb8d8a02391e0bac68c43197411e3f3230b
orinyostephen@gmail.com <orinyostephen@gmail.com>
2024-10-31 01:50:44 +0300

    Added the program code

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 5),
+            new Rectangle("Blue", 4, 6),
+            new Circle("Green", 3)
+        };
+
+        // Display color and area for each shape in the list
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Shape Area: {shape.GetArea():F2}");
+            Console.WriteLine();
+        }
     }
 }
